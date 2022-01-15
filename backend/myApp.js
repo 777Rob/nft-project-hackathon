78d321ce369f62b7ef94154ee8b0542008fd5ff3
Robertas <rcekanauskasnl@gmail.com>
2022-01-15 14:43:45 +0100

    Express, node

---
+++ backend/myApp.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+const fetch = require('node-fetch');
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+
+
+fetch('https://deep-index.moralis.io/api/v2/dateToBlock?chain=eth&date=1', {
+    method: 'GET',
+    headers: {'accept' : 'application/json', 'X-API-Key': process.env.API_KEY}
+}).then(json => console.log(json));
+
+
+
+ module.exports = app;
